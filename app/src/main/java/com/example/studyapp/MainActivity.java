618bf2d417ab2811677698751151d09fa58ab179
Caio Telles <cast@icomp.ufam.edu.br>
2020-02-22 19:21:59 -0400

    Update welcome activity

--- app/src/main/java/com/example/studyapp/MainActivity.java
+++ app/src/main/java/com/example/studyapp/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.studyapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.sobre:
+                AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                alert.setMessage("Login Transire").setNeutralButton("Tabom amor",null).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void entrarClicado(View view) {
+        Toast.makeText(this, "Olá!", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this,WelcomeActivity.class);
+        startActivity(intent);
     }
 }
